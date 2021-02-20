86ff2ccc608ee3f6ee6efd71c3465038df342d7d
MrDNA <jr.andreesen@gmail.com>
2021-02-20 20:14:35 +0100

    workshop upload

--- mod.lua
+++ mod.lua
@@ -2,13 +2,13 @@ atom ('mod.info',
 {
 	title = "Forestry",
 	description = "Tired of importing expensive wood just to please your aristocrats? Well, just grow your own trees then!\n\nThis is my first mod adding a new farm building to produce wood. Unfortunatley you can't use modded buildings in any of the default missions, so I also added a short example scenario to try out the new forestry.",
-	author = "shiranai",
-	homepage = "",
+	author = "shiranai (aka Mr.DNA)",
+	homepage = "https://github.com/MrDNA3712/Nebuchadnezzar-forestry-mod",
 	mod_version = "1.0",
 	game_version = "1.0.11",
 
 	-- Following lines are for external platforms (Steam) workshop mods only.
 	platform_public = true,
 	-- Do not change the following line. It's auto generated.
-	platform_id = "__mod_platform_id_not_set_yet__",
+	platform_id = "2402410735",
 })
